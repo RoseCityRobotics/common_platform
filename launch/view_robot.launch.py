714e20d5f38848c889cf6bcce8daba3015f485d0
Joe Cole <joe@rosecityrobotics.com>
2025-09-23 12:46:25 -0700

    Adjust and check static robot visualization

---
+++ launch/view_robot.launch.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    package_name = 'common_platform'
+    
+    # Launch robot state publisher
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(get_package_share_directory(package_name), 'launch', 'rsp.launch.py')
+        ])
+    )
+    
+    # Launch RViz2
+    rviz_config_file = os.path.join(
+        get_package_share_directory(package_name), 'config', 'view_robot.rviz'
+    )
+    
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config_file],
+        output='screen'
+    )
+    
+    # Joint State Publisher (for wheel joints)
+    joint_state_publisher = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        output='screen'
+    )
+    
+    return LaunchDescription([
+        rsp,
+        joint_state_publisher,
+        rviz
+    ])
