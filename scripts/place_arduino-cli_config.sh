5c5ef7f8efe0a3a638c125a7cd59ca5ec7272ba5
Joe Cole <joe@rosecityrobotics.com>
2025-09-30 15:09:09 -0700

    Add script to place arduino-cli config files

---
+++ scripts/place_arduino-cli_config.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/bash
+
+cp /home/${USER}/repos/common_platform/resources/boards.txt /home/${USER}/.arduino15/packages/teensy/hardware/avr/1.59.0/
+cp /home/${USER}/repos/common_platform/resources/platform.txt /home/${USER}/.arduino15/packages/teensy/hardware/avr/1.59.0/
+cp /home/${USER}/repos/common_platform/resources/installed.json /home/${USER}/.arduino15/packages/teensy/hardware/avr/1.59.0/
+cp /home/${USER}/repos/common_platform/resources/arduino-cli.yaml /home/${USER}/.arduino15/
+cp /home/${USER}/repos/common_platform/resources/library.properties /home/${USER}/Arduino/libraries/
+cp /home/${USER}/repos/common_platform/resources/library.properties /home/${USER}/Arduino/libraries/micro_ros_arduino/
